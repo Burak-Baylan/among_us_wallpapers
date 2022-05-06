b7de79f153ba25037626cb6125dc32a0bdc29b3c
Burak-Baylan <dsjkadnas@gmail.com>
2022-05-06 13:41:08 +0300

    update: main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:among_us_wallpapers/pages/home/view/home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +12,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Among Us Wallpaper'),
+        ),
+        body: buildFutureBuilder(),
       ),
-      home: Container(),
     );
+  }
+
+  Widget buildFutureBuilder() {
+    return FutureBuilder(
+      future: initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const HomeView();
+        }
+        if (snapshot.hasError) {
+          return buildErrorWidget();
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  Widget buildErrorWidget() {
+    return const Center(
+      child: Text(
+        'Something went wrong. Please restart app.',
+        style: TextStyle(
+          color: Colors.blueAccent,
+          fontSize: 45,
+        ),
+      ),
+    );
+  }
+
+  Future<void> initializeApp() async {
+    await Firebase.initializeApp();
   }
 }
