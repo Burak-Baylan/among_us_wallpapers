16140a7b64afc52c661502a84a81341eef67abac
Burak-Baylan <dsjkadnas@gmail.com>
2022-05-08 20:11:59 +0300

    add: add banner ad to home view

--- lib/pages/home/view/home_view.dart
+++ lib/pages/home/view/home_view.dart
@@ -1,8 +1,12 @@
-import 'package:among_us_wallpapers/pages/detail/view/details_view.dart';
-import 'package:among_us_wallpapers/pages/home/model/category_model.dart';
-import 'package:among_us_wallpapers/components/category_view_widget.dart';
-import 'package:among_us_wallpapers/pages/home/view_model/home_view_model.dart';
+import 'package:among_us_wallpapers/pages/buy_pro_mode_view/view/buy_pro_mode_view.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import '../../../app/ad_manager/ad_manager.dart';
+import '../../../components/category_view_widget.dart';
+import '../../../components/error_text.dart';
+import '../../detail/view/details_view.dart';
+import '../view_model/home_view_model.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -19,50 +23,75 @@ class _HomeViewState extends State<HomeView> {
   @override
   void initState() {
     super.initState();
+    homeVm.setContext(context);
     future = homeVm.getCategories();
+    WidgetsBinding.instance!.addPostFrameCallback(
+      (d) => homeVm.navigateToBuyPremium(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.hasError || homeVm.categories == null) {
-          return const Center(child: Text('Error'));
-        }
-        if (snapshot.connectionState == ConnectionState.done &&
-            homeVm.categories != null &&
-            homeVm.categories!.isEmpty) {
-          return const Center(child: Text('Categories not found'));
-        }
-        if (snapshot.connectionState == ConnectionState.done &&
-            homeVm.categories != null &&
-            homeVm.categories!.isNotEmpty) {
-          return listViewBuilder();
-        }
-        return const Center(child: CircularProgressIndicator());
-      },
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: FutureBuilder(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.hasError || homeVm.categories == null) {
+            return const ErrorText();
+          }
+          if (snapshot.connectionState == ConnectionState.done &&
+              homeVm.categories != null &&
+              homeVm.categories!.isNotEmpty) {
+            return Column(
+              children: [
+                Flexible(flex: 9, child: listViewBuilder()),
+                Flexible(flex: 1, child: AdWidget(ad: AdManager.homeBannerAd)),
+              ],
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: const Text('Among Us Wallpaper'),
+      actions: [
+        IconButton(
+          onPressed: () => homeVm.navigateToBuyPremium(),
+          icon: const Icon(Icons.workspace_premium_outlined),
+        ),
+      ],
     );
   }
 
   Widget listViewBuilder() {
     return ListView.builder(
       itemCount: homeVm.categories!.length,
+      shrinkWrap: true,
       itemBuilder: (context, index) {
         return CategoryViewWiget(
           title: homeVm.categories![index].title,
           link: homeVm.categories![index].link,
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    DetailsView(title: homeVm.categories![index].title),
-              ),
-            );
+            pushToDetailsView(index);
+            AdManager.showHomeScreenInterstitialAd();
           },
         );
       },
+    );
+  }
+
+  void pushToDetailsView(int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            DetailsView(title: homeVm.categories![index].title),
+      ),
     );
   }
 }
--- lib/pages/home/view_model/home_view_model.dart
+++ lib/pages/home/view_model/home_view_model.dart
@@ -1,16 +1,28 @@
-import 'package:among_us_wallpapers/app/helpers/category_getter.dart';
-import 'package:among_us_wallpapers/pages/home/model/category_model.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
+import '../../../app/helpers/category_getter.dart';
+import '../../buy_pro_mode_view/view/buy_pro_mode_view.dart';
+import '../model/category_model.dart';
 part 'home_view_model.g.dart';
 
 class HomeViewModel = _HomeViewModelBase with _$HomeViewModel;
 
 abstract class _HomeViewModelBase with Store {
   List<CategoryModel>? categories = [];
+  BuildContext? mContext;
+
+  void setContext(BuildContext context) => mContext = context;
 
   CategoryGetter categoryGetter = CategoryGetter();
 
   Future<void> getCategories() async {
     categories = await categoryGetter.getCategories();
+  }
+
+  void navigateToBuyPremium() {
+    Navigator.push(
+      mContext!,
+      MaterialPageRoute(builder: (context) => BuyProModeView()),
+    );
   }
 }
