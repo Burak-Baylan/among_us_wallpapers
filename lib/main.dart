72544429a3270b357b5f08af664c9b124f9bc280
Burak-Baylan <dsjkadnas@gmail.com>
2022-05-08 20:12:20 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
-import 'package:among_us_wallpapers/pages/home/view/home_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'app/ad_manager/ad_manager.dart';
+import 'components/error_text.dart';
+import 'pages/home/view/home_view.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Among Us Wallpapers',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Among Us Wallpaper'),
-        ),
         body: buildFutureBuilder(),
       ),
     );
@@ -31,26 +36,17 @@ class MyApp extends StatelessWidget {
           return const HomeView();
         }
         if (snapshot.hasError) {
-          return buildErrorWidget();
+          return const ErrorText();
         }
         return const Center(child: CircularProgressIndicator());
       },
     );
   }
 
-  Widget buildErrorWidget() {
-    return const Center(
-      child: Text(
-        'Something went wrong. Please restart app.',
-        style: TextStyle(
-          color: Colors.blueAccent,
-          fontSize: 45,
-        ),
-      ),
-    );
-  }
-
   Future<void> initializeApp() async {
     await Firebase.initializeApp();
+    await MobileAds.instance.initialize();
+    await AdManager.loadAllAds();
+    WidgetsFlutterBinding.ensureInitialized();
   }
 }
