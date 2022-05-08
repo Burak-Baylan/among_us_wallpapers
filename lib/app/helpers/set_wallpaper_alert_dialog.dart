f4469b9bf6b61f1ad7bbcef41ef59ccf8c788dc3
Burak-Baylan <dsjkadnas@gmail.com>
2022-05-08 20:11:08 +0300

    update: set wallpaper alert dialog

--- lib/app/helpers/set_wallpaper_alert_dialog.dart
+++ lib/app/helpers/set_wallpaper_alert_dialog.dart
@@ -1,31 +1,34 @@
-import 'package:among_us_wallpapers/app/helpers/wallpaper_helper.dart';
-import 'package:among_us_wallpapers/core/extensions/context_extensions.dart';
 import 'package:flutter/material.dart';
+import '../../core/extensions/context_extensions.dart';
+import '../ad_manager/ad_manager.dart';
+import 'wallpaper_helper.dart';
 
 class SetWallpaperAlertDialog {
   static Future<void> show(BuildContext context, String imageUrl) async {
     WallpaperHelper wallpaperHelper = WallpaperHelper();
     Widget setAsLockButton = TextButton(
-      child: Text("Set as Lock Screen"),
+      child: const Text("Set as Lock Screen"),
       onPressed: () async {
         await WallpaperHelper.setLockScreen(imageUrl);
+        await AdManager.showLockScreenInterstitialAd();
         context.pop;
       },
     );
     Widget setAsHomeButton = TextButton(
-      child: Text("Set as Home Screen"),
+      child: const Text("Set as Home Screen"),
       onPressed: () async {
         await WallpaperHelper.setHomeScreen(imageUrl);
+        await AdManager.showHomeScreenInterstitialAd();
         context.pop;
       },
     );
     Widget cancelButton = TextButton(
-      child: Text("Cancel"),
+      child: const Text("Cancel"),
       onPressed: () => context.pop,
     );
 
     AlertDialog alert = AlertDialog(
-      title: Text("Set Wallpaper"),
+      title: const Text("Set Wallpaper"),
       actions: [
         setAsHomeButton,
         setAsLockButton,
